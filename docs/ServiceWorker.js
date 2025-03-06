b6107235ffb116e12e05e2f5464cd4d677bea69b
IcamyS <105745467+icamysss@users.noreply.github.com>
2025-03-06 15:59:11 +0300

    buidl 2

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "DefaultCompany-BottleGame-1.0";
 const contentToCache = [
     "Build/docs.loader.js",
-    "Build/docs.framework.js.unityweb",
-    "Build/docs.data.unityweb",
-    "Build/docs.wasm.unityweb",
+    "Build/docs.framework.js",
+    "Build/docs.data",
+    "Build/docs.wasm",
     "TemplateData/style.css"
 
 ];
